cfd040bdee3d9d1a87763493f356a00aedbad803
Andy Sutherland <asutherland@pcs-publishing.com>
2023-09-25 16:40:05 +0000

    Insall and configure deno in devcontainer

--- README.md
+++ README.md
@@ -35,3 +35,13 @@ db: postgresql://postgres:postgres@localhost:54322/postgres
 studio: http://localhost:54323
 inbucket: http://localhost:54324
 ```
+
+### Edge Functions
+
+#### send-otp-email (send sign in code and link to supplied email address via inbucket)
+
+```bash
+POST: http://localhost:54321/functions/v1/send-otp-email
+
+data: {"email_address": "email@address.co.uk"}
+```
---
+++ supabase/functions/send-otp-email/index.ts
@@ -0,0 +1,45 @@
+import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+serve(async (req: Request) => {
+  try {
+    const { email_address } = await req.json();
+
+    if (!email_address) {
+      return throwError("Missing parameter: email_address is required.", 400);
+    }
+
+    const supabase = createClient(
+      // Supabase API URL - env var exported by default.
+      Deno.env.get("SUPABASE_URL") ?? "",
+      // Supabase API ANON KEY - env var exported by default.
+      Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+      // Create client with Auth context of the user that called the function.
+      // This way your row-level-security (RLS) policies are applied.
+      {
+        global: {
+          headers: { Authorization: req.headers.get("Authorization")! },
+        },
+      }
+    );
+
+    const { data } = await supabase.auth.signInWithOtp({
+      email: email_address,
+      options: {
+        emailRedirectTo: "http://localhost:3000",
+      },
+    });
+
+    console.log(data);
+
+    return new Response(JSON.stringify({ data }), {
+      headers: { "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { "Content-Type": "application/json" },
+      status: 400,
+    });
+  }
+});
