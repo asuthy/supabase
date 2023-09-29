7b0d283f225fbf9ff5775a70fea325b993811c69
Andy Sutherland <asutherland@pcs-publishing.com>
2023-09-29 15:09:42 +0000

    Wip on populating activities on sign in

--- supabase/migrations/20230928161146_Poulate members on new auth.sql
+++ supabase/migrations/20230928161146_Poulate members on new auth.sql
@@ -5,9 +5,16 @@ CREATE OR REPLACE FUNCTION public.on_auth_users_insert()
  LANGUAGE plpgsql
  SECURITY DEFINER
 AS $function$
+declare
+  member_id bigint;
 begin
   insert into copyfuse.members(email, user_id)
-  values (new.email, new.id);
+  values (new.email, new.id)
+  returning id into member_id;
+  
+  insert into copyfuse.activities(members, activity_category, activity_json)
+  values (member_id, 'auth.create', ROW_TO_JSON(new));
+
   return new;
 end;
 $function$
--- supabase/migrations/20230929092311_Set members.pending to false on first sign in.sql
+++ supabase/migrations/20230929092311_Set members.pending to false on first sign in.sql
@@ -5,12 +5,23 @@ CREATE OR REPLACE FUNCTION public.on_auth_users_update()
  LANGUAGE plpgsql
  SECURITY DEFINER
 AS $function$
+declare
+  member_id bigint;
 begin
-  update copyfuse.members
-  set pending = false
-  where user_id = new.id
-  and pending = true
-  and new.last_sign_in_at > old.last_sign_in_at;
+  if new.last_sign_in_at > old.last_sign_in_at then
+    update copyfuse.members
+    set pending = false
+    where user_id = new.id
+    and pending = true;
+
+    select id into member_id
+    from copyfuse_members
+    where user_id = new.id;
+
+    insert into copyfuse.activities(member_id, activity_category, activity_json)
+    values (new.id, 'auth.sign_in', ROW_TO_JSON(new));
+  end if;
+
   return new;
 end;
 $function$
