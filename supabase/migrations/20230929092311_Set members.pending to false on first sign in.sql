a8ccd4adf64a734df84029534aa7222f02616759
Andy Sutherland <asutherland@pcs-publishing.com>
2023-10-02 09:42:20 +0000

    Crete activities record on sign in

--- supabase/migrations/20230929092311_Set members.pending to false on first sign in.sql
+++ supabase/migrations/20230929092311_Set members.pending to false on first sign in.sql
@@ -8,18 +8,18 @@ AS $function$
 declare
   member_id bigint;
 begin
-  if new.last_sign_in_at > old.last_sign_in_at then
+  if (new.last_sign_in_at <> old.last_sign_in_at) or (new.last_sign_in_at is not null and old.last_sign_in_at is null) then
     update copyfuse.members
     set pending = false
     where user_id = new.id
     and pending = true;
 
     select id into member_id
-    from copyfuse_members
+    from copyfuse.members
     where user_id = new.id;
 
-    insert into copyfuse.activities(member_id, activity_category, activity_json)
-    values (new.id, 'auth.sign_in', ROW_TO_JSON(new));
+    insert into copyfuse.activities(members, activity_category, activity_json)
+    values (member_id, 'auth.sign_in', ROW_TO_JSON(new));
   end if;
 
   return new;
