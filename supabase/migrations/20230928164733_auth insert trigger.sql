830d2dda3998487f49b9e527b40827018fd252cf
Andy Sutherland <asutherland@pcs-publishing.com>
2023-10-03 09:27:37 +0000

    Fix for member pending not being set to false

--- supabase/migrations/20230928164733_auth insert trigger.sql
+++ supabase/migrations/20230928164733_auth insert trigger.sql
@@ -1,4 +1,4 @@
-create or replace trigger on_auth_users_insert
+create or replace trigger on_users_insert
 after insert on auth.users
 for each row
 execute function copyfuse.on_auth_users_insert();
--- supabase/migrations/20230929092311_Set members.pending to false on first sign in.sql
+++ supabase/migrations/20230929092311_Set members.pending to false on first sign in.sql
@@ -17,6 +17,11 @@ begin
   from copyfuse.members
   where user_id = sign_in_user_id;
 
+  update copyfuse.members
+  set pending = false
+  where user_id = sign_in_user_id
+  and pending = true;
+
   insert into copyfuse.activities(members, activity_category, activity_json)
   values (member_id, 'auth.sign_in', JSONB_BUILD_OBJECT('user_id', sign_in_user_id));
 
@@ -25,7 +30,7 @@ end;
 $function$
 ;
 
-create or replace trigger on_auth_mfa_amr_claims_insert
+create or replace trigger on_mfa_amr_claims_insert
 after insert on auth.mfa_amr_claims
 for each row
 execute function copyfuse.on_auth_mfa_amr_claims_insert();
