4f25ecef7c21e9643ae6013f987890b5f4595ca1
Andy Sutherland <asutherland@pcs-publishing.com>
2023-10-09 14:56:32 +0000

    Function to check that the auth user is a member of a network for rls

--- supabase/migrations/20231003142810_swiss_basic_rls.sql
+++ supabase/migrations/20231003142810_swiss_basic_rls.sql
@@ -1,23 +1,37 @@
+CREATE OR REPLACE FUNCTION copyfuse.is_users_networks_members(networks_param bigint, user_id_param uuid)
+ RETURNS boolean
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+begin  
+  if exists(  SELECT copyfuse.networks_members.networks
+              FROM copyfuse.networks_members,
+              copyfuse.members
+              where copyfuse.networks_members.members = copyfuse.members.id
+              and copyfuse.members.user_id = user_id_param
+              and copyfuse.networks_members.networks = networks_param
+              and copyfuse.networks_members.networks is not null) then
+    return true;
+  end if;
+
+  return false;
+end;
+$function$
+;
+
+GRANT EXECUTE ON FUNCTION copyfuse.is_users_networks_members TO authenticated;
+GRANT EXECUTE ON FUNCTION copyfuse.is_users_networks_members TO service_role;
+
 create policy "Members can create collections for networks they are associated to"
     on "swiss"."collections"
     as permissive
     for insert
     to public
-    with check (networks IN ( SELECT copyfuse.networks_members.networks
-    FROM copyfuse.networks_members,
-    copyfuse.members
-    where copyfuse.networks_members.members = copyfuse.members.id
-    and copyfuse.members.user_id = auth.uid()
-    and copyfuse.networks_members.networks is not null));
-
+    with check (copyfuse.is_users_networks_members(networks, auth.uid()));
+        
 create policy "Members can view collections for networks they are associated to"
     on "swiss"."collections"
     as permissive
     for select
     to public
-    using (networks IN ( SELECT copyfuse.networks_members.networks
-    FROM copyfuse.networks_members,
-    copyfuse.members
-    where copyfuse.networks_members.members = copyfuse.members.id
-    and copyfuse.members.user_id = auth.uid()
-    and copyfuse.networks_members.networks is not null));
+    using (copyfuse.is_users_networks_members(networks, auth.uid()));
