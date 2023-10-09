6800281fe46c2a71fed33481728a24f42ed41e4b
Josh Maguire <jmaguire@pcs-publishing.com>
2023-10-09 17:43:31 +0100

    Added policy for authenticated users to view activities

--- supabase/migrations/20231009163233_Added_policy_for_authenticated_users_to_view_activities.sql
+++ supabase/migrations/20231009163233_Added_policy_for_authenticated_users_to_view_activities.sql
@@ -4,9 +4,3 @@ as permissive
 for select
 to authenticated
 using (true);
-
-
-
-alter table "kis"."swiss_api_keys" alter column "key_id" set default '7cfba1f9-8551-498c-a63d-8d8864632e58'::uuid;
-
-
